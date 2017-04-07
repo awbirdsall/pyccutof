19af4ca3520d78ba8f2dbbfeda0b2571677d7ef9
awbirdsall <abirdsall@gmail.com>
2017-04-07 18:18:49 -0400

    add tests for ffc and fft functions
    - only check readffc and import_fft return values.
    - add returned ffc as pytest fixture.
    - use sample FFC and FFT files that I've been using for
      development. Put them in src/pyccutof/data and point to them
      in setup.py. Don't commit the binaries to the repo, at least for
      now, because the FFC file is big (~147 MB).

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=['pyccutof'],
     install_requires=['matplotlib>=1.5','numpy','scipy'],
     package_data={
-        # any data would go here
+        'pyccutof': ['data/sample.FFC', 'data/sample.FFT']
     },
     include_package_data=True,
     entry_points={
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,21 @@
 import pytest
 import pyccutof as ptof
 import numpy as np
+from pkg_resources import resource_filename
+
+@pytest.fixture(scope='module')
+def ffc():
+    fn = resource_filename('pyccutof', 'data/sample.FFC')
+    return ptof.readffc(fn)
 
 def test_upconvert_uint32_correct_calc():
     raw_bytes = np.array([0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa])
     output_words = np.array([0xddeeff, 0xaabbcc])
     assert np.equal(ptof.upconvert_uint32(raw_bytes, 3), output_words).all()
+
+def test_readffc_returns_value(ffc):
+    assert ffc
+
+def test_import_fft_returns_value(ffc):
+    fn = resource_filename('pyccutof', 'data/sample.FFT')
+    assert ptof.import_fft(fn, ffc[1])
